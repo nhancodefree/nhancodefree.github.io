1831ad2957678ae0b0f84d05d06cd74536156527
nhancodefree <102644098+nhancodefree@users.noreply.github.com>
2022-03-30 11:53:44 +0700

    Update ketnoiapi.php

--- apitrumdoithe/ketnoiapi.php
+++ apitrumdoithe/ketnoiapi.php
@@ -1,4 +1,4 @@
 <?php
-$trumdoitheApiKey = "uczq4iFgWMP0VfkRjd1tvoLIeb8Up9hQ"; //API BÊN NHANTHECAO.COM
+$trumdoitheApiKey = "BDtKCVHzmqf0GnNS3IMavWROhUp2jk7w"; //API BÊN NHANTHECAO.COM
 $callback_url = " tên miền /callback.php"; // THAY TÊN WEBSITE
 // LIÊN HỆ TUẤN ORI NẾU KHÔNG BIẾT LÀM
